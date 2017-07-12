0d985db2ece572b4f17a665212ad2ea4bb7fc758
Naookie Sato <nsato@cs.uoregon.edu>
2017-07-12 22:35:01 +0000

    auto pull cron job, for puppetmaster

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,9 @@
 node ip-10-0-6-22 {
+    cron { "puppet update":
+        command => "cd /etc/puppet && git pull -q origin master",
+        user    => root,
+        minute  => "*/5",
+    }
 }
 
 node ip-10-0-6-17 {
