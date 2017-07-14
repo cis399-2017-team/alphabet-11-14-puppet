7b56748dba91a2b22d360d9fef5b13160422b5e9
Naookie Sato <nsato@cs.uoregon.edu>
2017-07-14 18:51:38 +0000

    added node includes

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,12 +10,29 @@ node ip-10-0-6-22 {
         user    => root,
         minute  => "*/5",
     }
+
     include sshd
 
 }
 
 node ip-10-0-6-17 {
+    cron { "puppet agent autoconfigure":
+        command => "puppet agent -t",
+        user    => root,
+        minute  => "*/5",
+    }
+
+    include sshd
+
 }
 
 node ip-10-0-6-128 {
+    cron { "puppet agent autoconfigure":
+        command => "puppet agent -t",
+        user    => root,
+        minute  => "*/5",
+    }
+
+    include sshd
+
 }
