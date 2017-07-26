d8fe8f2b420d9f457ef77a51ed0e7a569723badc
Naookie Sato <nsato@cs.uoregon.edu>
2017-07-26 22:16:12 +0000

    defeated by puppet 3.8

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -5,7 +5,11 @@ class users {
         gid     => 3999,
     }
 
-    $useratt = {
+        
+
+    user { "nsato10":
+        ensure  => present,
+        uid     => 4000,
         gid     => 3999,
         password => "!",
         password_max_age => "99999",
@@ -14,33 +18,48 @@ class users {
         comment => "nsato10",
         purge_ssh_keys => true,
         managehome => true,
-    }
-
-    user { "nsato10":
-        ensure  => present,
-        uid     => 4000,
         home    => "/home/nsato10",
-        *       => $useratt,
     }
 
     user { "mheb":
         ensure  => present,
         uid     => 4001,
+        gid     => 3999,
+        password => "!",
+        password_max_age => "99999",
+        password_min_age => "0",
+        shell   => "/bin/bash",
+        comment => "mheb",
+        purge_ssh_keys => true,
+        managehome => true,
         home    => "/home/mheb",
-        *       => $useratt,
     }
 
     user { "loganr":
         ensure  => present,
         uid     => 4002,
+        gid     => 3999,
+        password => "!",
+        password_max_age => "99999",
+        password_min_age => "0",
+        shell   => "/bin/bash",
+        comment => "loganr",
+        purge_ssh_keys => true,
+        managehome => true,
         home    => "/home/loganr",
-        *       => $useratt,
     }
 
     user { "test":
         ensure  => absent,
         uid     => 4999,
+        gid     => 3999,
+        password => "!",
+        password_max_age => "99999",
+        password_min_age => "0",
+        shell   => "/bin/bash",
+        comment => "test",
+        purge_ssh_keys => true,
+        managehome => true,
         home    => "/home/test",
-        *       => $useratt,
     }
 }
