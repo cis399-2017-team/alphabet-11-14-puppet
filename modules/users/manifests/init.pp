58cdc51239974229a4c2f5a0751dd6ba3f6822d1
Naookie Sato <nsato@cs.uoregon.edu>
2017-07-26 04:49:46 +0000

    test user add

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -1,7 +1,21 @@
 class users {
+
+    group { "cis399":
+        ensure  => present,
+        gid     => 3999,
+        
+    }
+
     user { "nsato10":
         ensure  => present,
+        uid     => 4000,
+        gid     => 3999,
         home    => "/home/nsato10",
+        password => "!!",
+        password_max_age => "99999",
+        password_min_age => "0",
+        shell   => "/bin/bash",
+        comment => "nsato10",
         purge_ssh_keys => true,
     }
 
